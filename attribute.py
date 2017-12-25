5a4667f3726212fd0bed64ef49268fc23b2d0b54
Letian Chen <sunshinecltzac@gmail.com>
2017-12-26 02:43:14 +0800

    update attribute

--- attribute.py
+++ attribute.py
@@ -20,20 +20,21 @@ def transform_with_truth(truth_path, face_dir):
     truth = XMLOperation.read_from_file(truth_path)
 
     for _, image_name in enumerate(truth):
-        data = truth[image_name]
-        id, l, t, r, b = data[0], data[2], data[3], data[4], data[5]
+        datas = truth[image_name]
+        for data in datas:
+            id, l, t, r, b = data[0], data[2], data[3], data[4], data[5]
 
-        faces = position[image_name]
-        min_diff = 1e9
-        similar_face_index = None
-        for index, face in enumerate(faces):
-            diff = abs(data[2] - face[0]) + abs(data[3] - face[1]) + abs(data[4] - face[2]) + abs(data[5] - face[3])
-            if diff < min_diff:
-                diff = min_diff
-                similar_face_index = index
-        if min_diff < 100:
-            pic = cv2.imread(os.path.join(face_dir, image_name, str(similar_face_index) + ".png"))
-            cv2.imwrite(os.path.join(save_path, str(id), image_name), pic)
+            faces = position[image_name]
+            min_diff = 1e9
+            similar_face_index = None
+            for index, face in enumerate(faces):
+                diff = abs(data[2] - face[0]) + abs(data[3] - face[1]) + abs(data[4] - face[2]) + abs(data[5] - face[3])
+                if diff < min_diff:
+                    diff = min_diff
+                    similar_face_index = index
+            if min_diff < 100:
+                pic = cv2.imread(os.path.join(face_dir, image_name, str(similar_face_index) + ".png"))
+                cv2.imwrite(os.path.join(save_path, str(id), image_name), pic)
 
 
 transform_with_truth("/share/dataset/train/1_1_04_0/face recognize label result/dongnanmeneast_15_1920x1080_30.xml", "/mnt/disk/faces_east")
