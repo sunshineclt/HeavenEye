ed3b3e96298db4421c9561aeb8fae8ead31c8bf0
Letian Chen <sunshinecltzac@gmail.com>
2017-12-25 22:16:38 +0800

    detect all the face

--- detect.py
+++ detect.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import tensorflow as tf
+import os
 
 import FaceDetection
 
@@ -26,47 +27,53 @@ with tf.Graph().as_default():
     with sess.as_default():
         pnet, rnet, onet = FaceDetection.create_mtcnn(sess, './model_check_point/')
 
+path = "/share/dataset/train/1_1_04_0/prob/dongnanmeneast_15_1920x1080_30/"
+save_path = "/mnt/disk/faces/"
+# path = "."
+# save_path = "./test/"
 
-# ema = tf.train.ExponentialMovingAverage(1.0)
-# saver = tf.train.Saver(ema.variables_to_restore())
-# model_checkpoint_path = './model_check_point/model-20160506.ckpt-500000'
-# saver.restore(sess, model_checkpoint_path)
-ckpt = tf.train.get_checkpoint_state('./model_check_point/')
-saver = tf.train.import_meta_graph(ckpt.model_checkpoint_path + '.meta')
-saver.restore(sess, ckpt.model_checkpoint_path)
+files = os.listdir(path)
+for file_name in files:
+    if ".jpg" not in file_name:
+        continue
 
-pic = cv2.imread("test.jpg")
+    pic = cv2.imread(os.path.join(path, file_name))
 
-find_results = []
-gray = cv2.cvtColor(pic, cv2.COLOR_BGR2GRAY)
+    find_results = []
+    gray = cv2.cvtColor(pic, cv2.COLOR_BGR2GRAY)
 
-if gray.ndim == 2:
     img = to_rgb(gray)
 
-bounding_boxes, _ = FaceDetection.detect_face(img, minsize, pnet, rnet, onet, threshold, factor)
+    bounding_boxes, _ = FaceDetection.detect_face(img, minsize, pnet, rnet, onet, threshold, factor)
 
-number_of_faces = bounding_boxes.shape[0]  # number of faces
+    number_of_faces = bounding_boxes.shape[0]  # number of faces
 
-for face_position in bounding_boxes:
-    face_position = face_position.astype(int)
+    index = 0
+    img_dir = os.path.join(save_path, file_name)
+    if not os.path.exists(img_dir):
+        os.makedirs(img_dir)
+    for face_position in bounding_boxes:
+        face_position = face_position.astype(int)
 
-    cv2.rectangle(pic,
-                  (face_position[0],
-                   face_position[1]),
-                  (face_position[2],
-                   face_position[3]),
-                  (0, 255, 0), 2)
+        cv2.rectangle(pic,
+                      (face_position[0],
+                       face_position[1]),
+                      (face_position[2],
+                       face_position[3]),
+                      (0, 255, 0), 2)
 
-    crop = img[face_position[1]:face_position[3], face_position[0]:face_position[2], ]
-    crop = cv2.resize(crop, (96, 96), interpolation=cv2.INTER_CUBIC)
-    data = crop.reshape(-1, 96, 96, 3)
+        crop = img[face_position[1]:face_position[3], face_position[0]:face_position[2], ]
+        crop = cv2.resize(crop, (96, 96), interpolation=cv2.INTER_CUBIC)
+        cv2.imwrite(os.path.join(img_dir, str(index) + ".png"), crop)
+        index += 1
 
 
-# # Display the resulting frame
-# cv2.imshow('face_detection', pic)
-# while True:
-#     if cv2.waitKey(1) & 0xFF == ord('q'):
-#         break
-#
-# # When everything is done, release the capture
-# cv2.destroyAllWindows()
+
+        # Display the resulting frame
+        # cv2.imshow('face_detection', pic)
+        # while True:
+        #     if cv2.waitKey(1) & 0xFF == ord('q'):
+        #         break
+        #
+        # # When everything is done, release the capture
+        # cv2.destroyAllWindows()
