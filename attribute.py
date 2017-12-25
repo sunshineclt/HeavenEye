1de021901cdaf650958af7b4b2a7e86c412a6c41
Letian Chen <sunshinecltzac@gmail.com>
2017-12-26 02:53:22 +0800

    update attribute

--- attribute.py
+++ attribute.py
@@ -22,10 +22,9 @@ def transform_with_truth(truth_path, face_dir):
     for _, image_name in enumerate(truth):
         if image_name == "nothing":
             continue
-        print("Image name: {}".format(image_name))
         datas = truth[image_name]
         for data in datas:
-            id, l, t, r, b = data[0], data[2], data[3], data[4], data[5]
+            id = data[0]
 
             faces = position[image_name]
             min_diff = 1e9
@@ -33,11 +32,12 @@ def transform_with_truth(truth_path, face_dir):
             for index, face in enumerate(faces):
                 diff = abs(data[2] - face[0]) + abs(data[3] - face[1]) + abs(data[4] - face[2]) + abs(data[5] - face[3])
                 if diff < min_diff:
-                    diff = min_diff
+                    min_diff = diff
                     similar_face_index = index
             if min_diff < 100:
                 pic = cv2.imread(os.path.join(face_dir, image_name, str(similar_face_index) + ".png"))
                 cv2.imwrite(os.path.join(save_path, str(id), image_name), pic)
+                print("Image name: {} fit with face {} (ground truth id: {})".format(image_name, similar_face_index, id))
 
 
 transform_with_truth("/share/dataset/train/1_1_04_0/face recognize label result/dongnanmeneast_15_1920x1080_30.xml", "/mnt/disk/faces_east")
